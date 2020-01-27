73f65ccc04b86757c706891c2415c35670c2de02
RittnerE <erik-r-11@hotmail.de>
2020-01-27 16:21:29 +0100

    index.js fix

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+const path = require('path');
 const fs = require("fs");
 const express = require('express');
 const handlebars = require('express-handlebars');
 
 const app = express();
-
+const filePath = path.join(__dirname, "./messages.json");
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-
+app.set('views', path.join(__dirname, "./views"));
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (request, response) => {
-    const messagesText = fs.readFileSync("./messages.json", {encoding: "utf8"});
+    const messagesText = fs.readFileSync(filePath, {encoding: "utf8"});
     const messages = JSON.parse(messagesText);
     response.render('home', {messages: messages});
 });
 
 app.post('/new', (request, response) => {
-    const messages = fs.readFileSync("./messages.json", {encoding: "utf8"});
+    const messages = fs.readFileSync(filePath, {encoding: "utf8"});
     messages.push({
         author: request.body.author,
         text: request.body.text,
@@ -24,7 +25,7 @@ app.post('/new', (request, response) => {
     });
     response.redirect('/');
 });
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
